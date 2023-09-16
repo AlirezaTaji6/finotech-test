86ecee393f1ee4c772985f378833aadcaa0f6f2b
Alireza Taji <alirezataji6@gmail.com>
2023-09-16 21:19:12 +0330

    test: add create product e2e

--- test/products/create-product.e2e-spec.ts
+++ test/products/create-product.e2e-spec.ts
@@ -3,14 +3,15 @@ import { beforeAllUtil } from '../common/before-all';
 import request from 'supertest';
 import { RepositoryHelper } from '../common/repository-helper';
 import { ValidationTester } from '../common/validation-tester';
+import { HttpErrorByCode } from '@nestjs/common/utils/http-error-by-code.util';
 import { testAuthenticator } from '../common/authenticator';
 import { userSeeder } from '../seeders/user-seeder';
 import { testEmail } from '../common/constants';
 import { compare } from 'bcrypt';
 
-describe('Update User (e2e)', () => {
+describe('Create Product (e2e)', () => {
   let app: INestApplication;
-  const baseUrl = '/api/users';
+  const baseUrl = '/api/products';
   let repositoryHelper: RepositoryHelper;
   beforeAll(async () => {
     const result = await beforeAllUtil();
@@ -29,7 +30,7 @@ describe('Update User (e2e)', () => {
 
   it('Authentication failed', async () => {
     await request(app.getHttpServer())
-      .put(baseUrl)
+      .post(baseUrl)
       .send({})
       .expect(HttpStatus.UNAUTHORIZED);
   });
@@ -38,40 +39,40 @@ describe('Update User (e2e)', () => {
     const token = await testAuthenticator(app);
 
     const result = await request(app.getHttpServer())
-      .put(baseUrl)
+      .post(baseUrl)
       .set('Authorization', token)
       .send({})
       .expect(HttpStatus.BAD_REQUEST);
     new ValidationTester(result.body.message).isFirstLayer([
-      'firstName',
-      'lastName',
-      'password',
+      'title',
+      'description',
     ]);
   });
 
   it('ok', async () => {
     const token = await testAuthenticator(app);
 
-    const firstName = 'test_first_name';
-    const lastName = 'test_last_name';
-    const password = 'changed_password';
+    const title = 'test_title';
+    const description = 'test_description';
 
     await request(app.getHttpServer())
-      .put(baseUrl)
+      .post(baseUrl)
       .set('Authorization', token)
       .send({
-        firstName,
-        lastName,
-        password,
+        title,
+        description,
       })
-      .expect(HttpStatus.OK);
+      .expect(HttpStatus.CREATED);
+
+    const productFound = await repositoryHelper.repository.product.findOne({
+      where: {},
+    });
+    expect(productFound.title).toBe(title);
+    expect(productFound.description).toBe(description);
 
     const userFound = await repositoryHelper.repository.user.findOne({
       where: { email: testEmail },
     });
-    expect(userFound.firstName).toBe(firstName);
-    expect(userFound.lastName).toBe(lastName);
-    const isPasswordChanged = await compare(password, userFound.password);
-    expect(isPasswordChanged).toBe(true);
+    expect(userFound.id).toBe(productFound.userId);
   });
 });
